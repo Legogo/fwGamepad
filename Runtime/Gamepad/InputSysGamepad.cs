8c5ef5f254a53fca7dab35db8f10553d2174a749
ab <andre.berlemont@gmail.com>
2024-08-20 14:45:32 +0200

    clean namings

--- Runtime/Gamepad/InputSysGamepad.cs
+++ Runtime/Gamepad/InputSysGamepad.cs
@@ -393,19 +393,5 @@ namespace fwp.gamepad
     }
 
 
-    /// <summary>
-    /// used by various elements
-    /// </summary>
-    public class InputSubsCallbacks
-    {
-        public Action<InputJoystickSide, Vector2> onJoystickDirection; // only when value change beyond a threshold
-        public Action<InputJoystickSide, Vector2> onJoystickPerformed; // every frame the value changes
-        public Action<InputJoystickSide> onJoystickReleased; // joystick went back to neutral
-
-        public Action<InputButtons, bool> onButtonPerformed;
-        public Action<InputTriggers, float> onTriggerPerformed;
-        public Action<InputDPad, bool> onDPadPerformed;
-    }
-
 
 }
